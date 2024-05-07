c97ffef8e10fbec17b58cf735048b950193f84b6
hugofluhr <hugo.fluhr@gmail.com>
2024-05-07 12:37:45 +0200

    better script to check data up to current date

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 import datetime
 import re
 import os
+import pprint
 
 file_patterns = {'rewardpairs': [
                             r'habit_{}_1_2024-.{{11}}.mat',
@@ -21,13 +22,15 @@ def search_patterns(subject_id, directory, patterns):
 
     Parameters:
     - subject_id (str): The ID of the subject.
+    - directory (str): The directory to search for files.
     - patterns (list): A list of file name patterns to search for.
 
     Returns:
     - found_files (dict): A dictionary where the keys are the patterns and the values are lists of found file names.
 
     Example:
-    >>> subject_id = '001'
+    >>> subject_id = 'aaaaaa'
+    >>> directory = '/path/to/directory'
     >>> patterns = ['data_{}.csv', 'results_{}.txt']
     >>> search_patterns(subject_id, directory, patterns)
     {'data_{}.csv': ['data_001.csv'], 'results_{}.txt': []}
@@ -35,12 +38,12 @@ def search_patterns(subject_id, directory, patterns):
     files = os.listdir(directory)
     found_files = {pattern: [] for pattern in patterns}
     
-    for file in files:
-        for pattern in patterns:
-            pattern_with_id = pattern.format(re.escape(subject_id))
-            if re.match(pattern_with_id, file, re.IGNORECASE):
-                found_files[pattern].append(file)
-    
+    for pattern in patterns:
+        pattern_with_id = pattern.format(re.escape(subject_id))
+        matches = [file for file in files if re.match(pattern_with_id, file, re.IGNORECASE)]
+        if matches:
+            found_files[pattern] = matches
+
     return found_files
 
 def check_data(subject_id, exp_data_dir, file_patterns):
@@ -53,45 +56,42 @@ def check_data(subject_id, exp_data_dir, file_patterns):
         file_patterns (dict): A dictionary containing file patterns for different types of data.
 
     Returns:
-        None
+        incomplete_data (dict): A dictionary containing the types of data that are incomplete for the subject.
+
+    Example:
+    >>> subject_id = 'aaaaaa'
+    >>> exp_data_dir = '/path/to/experiment/data'
+    >>> file_patterns = {'rewardpairs': [r'habit_{}_1_2024-.{{11}}.mat'], 'gonogo': [r'results_{}.mat']}
+    >>> check_data(subject_id, exp_data_dir, file_patterns)
+    {'001': {'rewardpairs': ['habit_001_1_2024-01-01.mat'], 'gonogo': []}}
     """
+    incomplete_data = {}
 
     # Reward pairs data
     reward_pairs_dir = os.path.join(exp_data_dir, 'data')
     reward_pairs_files = search_patterns(subject_id, reward_pairs_dir, file_patterns['rewardpairs'])
-    if all(len(files) == 1 for files in reward_pairs_files.values()):
-        #print("     Reward pairs data complete!")
-        pass
-    else:
-        print("\tReward pairs data incomplete!")
-        for pattern, files in reward_pairs_files.items():
-            if len(files) != 1:
-                print(f"        Pattern {pattern} has {len(files)} matches.")
-                for file in files:
-                    print(file)
+    if not all(len(files) == 1 for files in reward_pairs_files.values()):
+        if subject_id not in incomplete_data:
+            incomplete_data[subject_id] = {}
+        incomplete_data[subject_id]['rewardpairs'] = [pattern for pattern, files in reward_pairs_files.items() if len(files) != 1]
 
     # GoNoGo data
     gonogo_dir = os.path.join(exp_data_dir, 'data_gonogo')
     gonogo_files = search_patterns(subject_id, gonogo_dir, file_patterns['gonogo'])
-    if len(gonogo_files['results_{}.mat']) == 1:
-        #print("     GoNoGo data complete!")
-        pass
-    else:
-        print("\tGoNoGo data incomplete!")
-        for file in gonogo_files['results_{}.mat']:
-            print(file)
+    if not len(gonogo_files['results_{}.mat']) == 1:
+        if subject_id not in incomplete_data:
+            incomplete_data[subject_id] = {}
+        incomplete_data[subject_id]['gonogo'] = f'results_{subject_id}.mat'
 
     # Eye tracking data
     eyetracking_dir = os.path.join(exp_data_dir, 'data_eyetracker')
     eyetracking_files = search_patterns(subject_id, eyetracking_dir, file_patterns['eyetracking'])
-    if len(eyetracking_files['{}.edf']) == 1:
-        #print("     Eye tracking data complete!")
-        pass
-    else:
-        print("\tEye tracking data incomplete!")
-        for file in eyetracking_files['{}.edf']:
-            print(file)
+    if not len(eyetracking_files['{}.edf']) == 1:
+        if subject_id not in incomplete_data:
+            incomplete_data[subject_id] = {}
+        incomplete_data[subject_id]['eyetracking'] = f'{subject_id}.edf'
 
+    return incomplete_data
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Check data for subjects.')
@@ -102,8 +102,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     subj_info = pd.read_excel(args.participants_info_path)
-    acquired = subj_info[subj_info["Date MRI"] < datetime.date.today().strftime("%Y-%m-%d")]
+    acquired = subj_info[subj_info["Date MRI"] <= datetime.date.today().strftime("%Y-%m-%d")]
     acquired_subj = acquired['ID'].str.lower().values
+
+    complete_participants = []
+    incomplete_data_dict = {}
 
     for sub in acquired_subj:
         # Check that the subject ID is a string of length 6
@@ -111,4 +114,14 @@ if __name__ == '__main__':
 
         # Checking data for each subject
         print(f"Checking data for subject {sub}")
-        check_data(sub, args.exp_data_dir, file_patterns)
+        incomplete_data = check_data(sub, args.exp_data_dir, file_patterns)
+        if not incomplete_data:
+            complete_participants.append(sub)
+        else:
+            incomplete_data_dict[sub] = incomplete_data
+
+    print("\nComplete data participants:")
+    print(complete_participants)
+
+    print("\nMissing data:")
+    pprint.pprint(incomplete_data_dict, indent=4)
