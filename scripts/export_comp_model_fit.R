c96144cb34eaed655804f28e06dd2d1b6488db7f
hugofluhr <hugo.fluhr@gmail.com>
2024-10-01 22:46:06 +0200

    R script to convert modeling data to csv

---
+++ scripts/export_comp_model_fit.R
@@ -0,0 +1,31 @@
+R_file_path <- "/Users/hugofluhr/phd_local/repositories/Learning-Habits-Behavioral-Analyses/data_RP_MRI_model_RL_CK_reduced.RData"
+output_directory <- "/Users/hugofluhr/data/LH_dev/modeling_data/"
+
+# Load the RData file
+load(R_file_path)  # Replace with your actual .RData file path
+
+# Function to split a data frame by the first column and save the splits as CSV
+split_and_save <- function(dataframe, dfname, output_directory) {
+  # Get the unique values in the first column
+  subject_ids <- unique(dataframe[[1]])
+  
+  # Loop over each unique value, split, and save
+  for (subject in subject_ids) {
+    # Subset the dataframe based on the first column value
+    subset_df <- subset(dataframe, dataframe[[1]] == subject)
+    
+    # Generate a filename based on the data frame name and the value in the first column
+    filename <- tolower(paste0(subject,"_", dfname, ".csv"))
+    
+    # Save the subset to a CSV file
+    write.csv(subset_df, file = file.path(output_directory,filename), row.names = FALSE)
+    
+    # Print a message to indicate the file has been saved
+    cat("Saved:", filename, "\n")
+  }
+}
+
+
+# Applying the function to both learning and test data
+split_and_save(data_test_RL_CK2_extended, "test_compmodel2709", output_directory)  
+split_and_save(data_train_RL_CK2_extended, "learning_compmodel2709", output_directory)
