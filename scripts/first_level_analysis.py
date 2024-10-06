5e086d103df34d962103426c21c2252ec5425abc
hugofluhr <hugo.fluhr@gmail.com>
2024-10-06 19:22:18 +0200

    run 1st level on all subjects

--- scripts/first_level_analysis.py
+++ scripts/first_level_analysis.py
@@ -2,13 +2,13 @@ import sys
 import os
 from nilearn.interfaces.fmriprep import load_confounds
 sys.path.append('/Users/hugofluhr/phd_local/repositories/RewardPairsTask_Analysis/')
-from utils.data import Subject
+from utils.data import Subject, load_participant_list
 from utils.analysis import run_model_rl, run_model_ck
 
 # Set base directory and derivatives directory
 base_dir = '/Users/hugofluhr/data/LH_dev'
 bids_dir = "/Users/hugofluhr/data/LH_dev/fmriprep-23.2.1"
-derivatives_dir = os.path.join(base_dir, 'nilearn_first_level')
+derivatives_dir = os.path.join(base_dir, 'nilearn_first_level_no_baseline_masked')
 
 # Create derivatives folder if it does not exist
 if not os.path.exists(derivatives_dir):
@@ -21,12 +21,14 @@ noise_model = 'ar1'
 smoothing_fwhm = 5
 high_pass = 0.01
 motion_type = 'basic'
+mask_samples = True
 
 # Function to load subject data including confounds
 def load_subject_data(sub_id):
     subject = Subject(base_dir, sub_id, include_modeling=True, include_imaging=True, bids_dir=bids_dir)
     
     confounds_dict = {}
+    sample_mask_dict = {}
     for run in subject.runs:
         img_path = subject.img.get(run)
         confounds, sample_mask = load_confounds(
@@ -43,15 +45,24 @@ def load_subject_data(sub_id):
         cosine_columns_to_keep = cosine_columns[:5]
         columns_to_keep = [col for col in confounds.columns if not col.startswith('cosine')] + cosine_columns_to_keep
         confounds_dict[run] = confounds[columns_to_keep]
+        sample_mask_dict[run] = sample_mask
 
-    return subject, confounds_dict
+    return subject, confounds_dict, sample_mask_dict
 
 
 # Entry point for the script
 if __name__ == "__main__":
-    sub_id = '01'  # Example subject ID list
-    subject, confounds_dict = load_subject_data(sub_id)
+    subject_ids = load_participant_list(base_dir)
 
-    for run in subject.runs:
-        run_model_rl(subject, run, confounds_dict[run], tr, hrf_model, high_pass, smoothing_fwhm, derivatives_dir, plot_stat=False, plot_design=True)
-        run_model_ck(subject, run, confounds_dict[run], tr, hrf_model, high_pass, smoothing_fwhm, derivatives_dir, plot_stat=False, plot_design=True)
+    for sub_id in subject_ids:
+        print(f"Processing Subject {sub_id}...")
+        try:
+            subject, confounds_dict, sample_mask = load_subject_data(sub_id)
+            if not mask_samples:
+                sample_mask = {run: None for run in subject.runs}
+            for run in subject.runs:
+                run_model_rl(subject, run, confounds_dict[run], sample_mask[run],tr, hrf_model, high_pass, smoothing_fwhm, derivatives_dir, remove_baseline=True, plot_stat=False, plot_design=True)
+                run_model_ck(subject, run, confounds_dict[run], sample_mask[run], tr, hrf_model, high_pass, smoothing_fwhm, derivatives_dir, remove_baseline=True, plot_stat=False, plot_design=True)
+        
+        except Exception as e:
+            print(f"An error occurred for Subject {sub_id}: {e}")
