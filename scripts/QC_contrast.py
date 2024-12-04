7ac19d8217ac4f0184b08b7d9439a49d14c0475c
Hugo Fluhr <hugo.fluhr@gmail.com>
2024-12-04 16:02:31 +0000

    working on QC analysis

--- scripts/QC_contrast.py
+++ scripts/QC_contrast.py
@@ -14,7 +14,7 @@ import json
 # Set base directory and derivatives directory
 base_dir = '/home/ubuntu/data/learning-habits'
 bids_dir = '/home/ubuntu/data/learning-habits/bids_dataset/derivatives/fmriprep-23.2.1/'
-derivatives_dir = os.path.join(base_dir, 'outputs/first_level/QC_cloud-23.2.1')
+derivatives_dir = os.path.join(base_dir, 'outputs/first_level/QC_cloud-23.2.1_brain_masked')
 
 # Create derivatives folder if it does not exist
 if not os.path.exists(derivatives_dir):
@@ -29,6 +29,7 @@ parameters = {
     "high_pass": 0.01,
     "motion_type": 'basic',
     "mask_samples": True,
+    "brain_mask": True,
     "demean_modulators": True
 }
 
@@ -45,9 +46,10 @@ smoothing_fwhm = parameters["smoothing_fwhm"]
 high_pass = parameters["high_pass"]
 motion_type = parameters["motion_type"]
 mask_samples = parameters["mask_samples"]
+brain_mask = parameters["brain_mask"]
 demean_modulators = parameters["demean_modulators"]
 
-def process_subject(sub_id, tr, hrf_model, high_pass, smoothing_fwhm, derivatives_dir, mask_samples):
+def process_subject(sub_id, tr, hrf_model, high_pass, smoothing_fwhm, derivatives_dir, mask_samples, brain_mask):
     print(f"Processing Subject {sub_id}...")  
     model_label = "response_QC"
     try:
@@ -58,11 +60,14 @@ def process_subject(sub_id, tr, hrf_model, high_pass, smoothing_fwhm, derivative
                 sample_mask = None
             img_path = subject.img.get(run)
             fmri_img = image.load_img(img_path)
+            if brain_mask:
+                brain_mask_path = subject.brain_mask.get(run)
+                brain_mask = image.load_img(brain_mask_path)
+            else:
+                brain_mask = None
+
             n = fmri_img.shape[-1]
             frametimes = np.linspace(tr/2., (n - .5)*tr, n) # from Gilles, checked with nilearn FirstLevelModel
-            # if sample_mask is not None, change the model label.
-            if sample_mask is not None:
-                 model_label = f"{model_label}_masked"
 
             # Ignore warnings related to null duration events and unexpected columns in events data
             warnings.filterwarnings("ignore", message=".*events with null duration.*")
@@ -74,13 +79,13 @@ def process_subject(sub_id, tr, hrf_model, high_pass, smoothing_fwhm, derivative
                                                 drift_model=None,
                                                 high_pass=high_pass,
                                                 add_regs=confounds)
-            model = FirstLevelModel(smoothing_fwhm=smoothing_fwhm)
-            model_fit = model.fit(fmri_img, design_matrices=X1, sample_masks=sample_mask)
+            model = FirstLevelModel(smoothing_fwhm=smoothing_fwhm, mask_img=brain_mask)
+            model = model.fit(fmri_img, design_matrices=X1, sample_masks=sample_mask)
             # Compute betamap and save it
             z_map = model.compute_contrast(
-                contrast_def=f"{'response'}", output_type="z_score"
+                contrast_def=f"{'response'}", output_type="effect_size"
             )
-            z_map_path = os.path.join(derivatives_dir, f'{subject.sub_id}_run-{run}_{model_label}_z_map.nii.gz')
+            z_map_path = os.path.join(derivatives_dir, f'{subject.sub_id}_run-{run}_{model_label}_b_map.nii.gz')
             z_map.to_filename(z_map_path)
             print(f"{model_label.capitalize()} betamap results saved to {z_map_path}")
 
@@ -97,7 +102,8 @@ if __name__ == "__main__":
     # Set up parallel processing with ProcessPoolExecutor
     with ProcessPoolExecutor(max_workers=8) as executor:
         # Submit all subjects to be processed
-        futures = {executor.submit(process_subject, sub_id, tr, hrf_model, high_pass, smoothing_fwhm, derivatives_dir, mask_samples): sub_id for sub_id in subject_ids}
+        futures = {executor.submit(process_subject, sub_id, tr, hrf_model, high_pass, smoothing_fwhm,
+                                    derivatives_dir, mask_samples, brain_mask): sub_id for sub_id in subject_ids}
         
         # Gather results as they are completed
         for future in as_completed(futures):
