145912b84f48d80b6b789a0ade9282756a0e3a05
hugofluhr <hugo.fluhr@gmail.com>
2025-06-04 20:19:06 +0000

    new script for spm smoothing

--- matlab/spm_smooth_data.m
+++ matlab/spm_smooth_data.m
@@ -1,19 +1,10 @@
-%addpath('/home/ubuntu/repos/spm12');
+addpath('/home/ubuntu/repos/spm12');
 
 spm('Defaults', 'fMRI');
 spm_jobman('initcfg');
 
 % Directory paths
-base_dir = '/Users/hugofluhr/phd_local/data/LearningHabits/dev_sample/bids_dataset';
-fmriprep_dir = dir(fullfile(base_dir, 'derivatives', 'fmriprep*'));
-if isempty(fmriprep_dir)
-    error('No directory starting with "fmriprep" found in base_dir.');
-end
-fmriprep_dir = fullfile(base_dir, 'derivatives', fmriprep_dir(1).name);
-spm_dir = fullfile(base_dir, 'derivatives', 'spm'); 
-if ~exist(spm_dir, 'dir')
-    mkdir(spm_dir);
-end
+base_dir = '/home/ubuntu/data/learning-habits/spm_format_20250603';
 
 % Parameters
 smoothing_fwhm = 5; % Smoothing kernel
@@ -26,13 +17,20 @@ subjects = {sub_dirs([sub_dirs.isdir]).name}; % Get all sub- directory names
 for i = 1:length(subjects)
     sub_id = subjects{i};
     disp(['Processing subject: ', sub_id]);
-    func_dir = fullfile(fmriprep_dir, sub_id, 'ses-1','func');
+
+    func_dir = fullfile(base_dir, sub_id,'func');
     disp(func_dir);
     
     % Identify unique runs based on BOLD file names
     bold_files = spm_select('FPList', func_dir, '^sub-.*_desc-preproc_bold.nii$');
     run_ids = unique(regexp(cellstr(bold_files), 'run-\d+', 'match', 'once'));
     
+    % Check for BOLD files
+    if isempty(bold_files)
+        warning(['No BOLD files found for ' sub_id]);
+        continue;
+    end
+
     % Step 1: Smoothing
     for j = 1:length(run_ids)
         run_id = run_ids{j};
@@ -40,7 +38,8 @@ for i = 1:length(subjects)
         % Select BOLD files for current run
         run_bold_files = spm_select('FPList', func_dir, ['^sub-.*' run_id '_.*_desc-preproc_bold.nii$']);
         if isempty(run_bold_files)
-            error(['No BOLD files found for run: ', run_id, ' in subject: ', sub_id]);
+            warning(['    No BOLD file found for run: ' run_id]);
+            continue;
         end
         
         % Smoothing
@@ -53,10 +52,5 @@ for i = 1:length(subjects)
         
         % Save and run smoothing batch
         spm_jobman('run', matlabbatch);
-    end
-    % Move smoothed files
-    smoothed_files = dir(fullfile(source_dir, 'smoothed_*.nii'));
-    for i = 1:length(smoothed_files)
-        movefile(fullfile(source_dir, smoothed_files(i).name), target_dir);
     end
 end
---
+++ scripts/spm_smooth.sh
@@ -0,0 +1,3 @@
+module load matlab
+matlab -nodisplay -nosplash -nodesktop -r \
+"run('/home/ubuntu/repos/learning-habits-analysis/matlab/spm_smooth_data.m'); exit;"
