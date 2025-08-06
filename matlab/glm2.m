c24d8f75ef91d58292e4d597c4be16e4b017a3ed
hugofluhr <hugo.fluhr@gmail.com>
2025-08-06 16:33:37 +0000

    glm2 [wip] - big update, more contrasts

--- matlab/glm2.m
+++ matlab/glm2.m
@@ -28,8 +28,11 @@ TR = 2.33384; % Repetition time
 high_pass_cutoff = 128; % High-pass filter in seconds
 
 % set up contrasts
-connames = {'first_stim', 'second_stim', 'second_stimxQval_diff', 'second_stimxHval_diff', ...
-'response', 'feedback'};
+connames = {
+    'first_stim', 'first_stimxQval', 'first_stimxHval', ...
+    'second_stim', 'second_stimxQval', 'second_stimxHval', ...
+    'response', 'feedback'
+    };
 
 spm('Defaults', 'fMRI');
 spm_jobman('initcfg');
@@ -42,7 +45,6 @@ for s = 1:length(subjects)
     end
     disp(['Processing subject: ', sub_id]);
     func_dir = fullfile(data_dir, sub_id, 'func');
-    disp(func_dir);
     
     % Identify unique runs based on BOLD file names
     bold_files = spm_select('FPList', func_dir, '^sub-.*_desc-preproc_bold.nii$');
@@ -82,9 +84,15 @@ for s = 1:length(subjects)
         block_data = bbt(strcmp(bbt.sub_id, sub_id) & strcmp(bbt.block, block_names{r}), :);
         % Ignore non response trials
         block_data = block_data(~isnan(block_data.action), :);
-
+        % Ignore trials with highest/lowest stimuli
+        % Create a mask for excluding specific trials
+        trial_mask = (block_data.first_stim ~= 1) & (block_data.first_stim ~= 8);
+        block_incl = block_data(trial_mask,:);
+        block_excl = block_data(~trial_mask,:);
         
-        % Model specification for this run
+        %% -----------------
+        % 1) SPEC + EST BATCH
+        %% -----------------
         clear matlabbatch
         matlabbatch{1}.spm.stats.fmri_spec.dir = {run_output_dir};
         matlabbatch{1}.spm.stats.fmri_spec.timing.units = 'secs';
@@ -102,21 +110,35 @@ for s = 1:length(subjects)
         
         % First stimulus - included 
         matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).name = 'first_stim';
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).onset = block_data.t_first_stim;
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).duration = block_data.t_second_stim - block_data.t_first_stim; % check duration
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).onset = block_incl.t_first_stim;
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).duration = block_incl.t_second_stim - block_incl.t_first_stim; % check duration
         matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).tmod = 0;   
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).pmod(1).name = 'Qval';
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).pmod(1).param = zscore(block_incl.first_stim_value_rl);
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).pmod(1).poly = 1;
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).pmod(2).name = 'Hval';
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).pmod(2).param = zscore(block_incl.first_stim_value_ck);
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).pmod(2).poly = 1;
         matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).orth = 0;
+        
+        % First stimulus - excluded
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(5).name = 'first_stim_excl';
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(5).onset = block_excl.t_first_stim;
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(5).duration = block_excl.t_second_stim - block_excl.t_first_stim;
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(5).tmod = 0;   
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(5).pmod = struct('name', {}, 'param', {}, 'poly', {});
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(5).orth = 0;
 
         % Second stimulus
         matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).name = 'second_stim';
         matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).onset = block_data.t_second_stim;
         matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).duration = block_data.t_action - block_data.t_second_stim; % check duration
         matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).tmod = 0;
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).pmod(1).name = 'Qval_diff';
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).pmod(1).param = zscore(block_data.first_stim_value_rl-block_data.second_stim_value_rl);
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).pmod(1).name = 'Qval';
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).pmod(1).param = zscore(block_data.second_stim_value_rl);
         matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).pmod(1).poly = 1;
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).pmod(2).name = 'Hval_diff';
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).pmod(2).param = zscore(block_data.first_stim_value_ck-block_data.second_stim_value_ck);
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).pmod(2).name = 'Hval';
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).pmod(2).param = zscore(block_data.second_stim_value_ck);
         matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).pmod(2).poly = 1;
         matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).orth = 0;
         
@@ -156,27 +178,51 @@ for s = 1:length(subjects)
         matlabbatch{2}.spm.stats.fmri_est.write_residuals = 0;
         matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
         
-        %% Contrast definition
-        matlabbatch{3}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
-        
-        for cc = 1:length(connames)
-            matlabbatch{3}.spm.stats.con.consess{cc}.tcon.name = connames{cc};
-            matlabbatch{3}.spm.stats.con.consess{cc}.tcon.weights = [zeros(1,cc-1) 1];
-            matlabbatch{3}.spm.stats.con.consess{cc}.tcon.sessrep = 'none';
-        end
-        
-        matlabbatch{3}.spm.stats.con.delete = 1;
-        
-        % Save and run batch
-        save(fullfile(run_output_dir, ['batch_job_', run_id, '.mat']), 'matlabbatch');
+        save(fullfile(run_output_dir, ['batch_spec_est_', run_id, '.mat']), 'matlabbatch');
         spm_jobman('run', matlabbatch);
         
-        %% Saving regressor names for traceability
-        % Load SPM.mat to extract regressor names
+        %% -----------------
+        % 2) CONTRAST BATCH
+        %% -----------------
         SPM_path = fullfile(run_output_dir, 'SPM.mat');
         load(SPM_path, 'SPM');
         
-        % Save regressor names
+        matlabbatch_con = [];
+        matlabbatch_con{1}.spm.stats.con.spmmat = {SPM_path};
+        
+        for cc = 1:length(connames)
+            idx = find(contains(SPM.xX.name, connames{cc}));
+            if isempty(idx)
+                warning('No regressor found for %s in %s', connames{cc}, run_id);
+                continue
+            end
+            weights = zeros(1, length(SPM.xX.name));
+            weights(idx) = 1;
+            matlabbatch_con{1}.spm.stats.con.consess{cc}.tcon.name = connames{cc};
+            matlabbatch_con{1}.spm.stats.con.consess{cc}.tcon.weights = weights;
+            matlabbatch_con{1}.spm.stats.con.consess{cc}.tcon.sessrep = 'none';
+        end
+
+        % Add custom extra contrasts
+        extra_contrasts = {
+            'Qval_sum',  [0 1 0 0 1 0];
+            'Hval_sum',  [0 0 1 0 0 1];
+            'Qval_diff', [0 1 0 0 -1 0];
+            'Hval_diff', [0 0 1 0 0 -1]
+        };
+        % Append each extra contrast
+        for ec = 1:size(extra_contrasts, 1)
+            matlabbatch_con{1}.spm.stats.con.consess{end+1}.tcon.name    = extra_contrasts{ec, 1};
+            matlabbatch_con{1}.spm.stats.con.consess{end}.tcon.weights   = extra_contrasts{ec, 2};
+            matlabbatch_con{1}.spm.stats.con.consess{end}.tcon.sessrep   = 'none';
+        end
+
+        matlabbatch_con{1}.spm.stats.con.delete = 1;
+        
+        save(fullfile(run_output_dir, ['batch_contrasts_', run_id, '.mat']), 'matlabbatch_con');
+        spm_jobman('run', matlabbatch_con);
+        
+        %% Save regressor names
         reg_names = SPM.xX.name;
         save(fullfile(run_output_dir, 'regressor_names.mat'), 'reg_names');
         
