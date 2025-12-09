9b38806272215d83bdd2e92a44e85e708ae7b99b
hugofluhr <hugo.fluhr@gmail.com>
2025-12-09 19:38:43 +0000

    glm that combines all 3 runs

--- scripts/spm_first_lvl.sh
+++ scripts/spm_first_lvl.sh
@@ -1,4 +1,4 @@
 module load matlab
-timestamp=$(date +%Y%m%d_%H%M%S)
-matlab -nodisplay -nosplash -nodesktop -r \
-"run('/home/ubuntu/repos/learning-habits-analysis/matlab/first_lvl/glm2_combined.m'); exit;"
+matlab -nodisplay -nosplash -nodesktop -r "\
+run('/home/ubuntu/repos/learning-habits-analysis/matlab/first_lvl/glm2_all_runs.m'); \
+exit;"
