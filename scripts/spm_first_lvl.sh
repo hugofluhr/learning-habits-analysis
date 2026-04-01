950a13014b3331bdfd5e416b6d56237ddae41264
hugofluhr <hugo.fluhr@gmail.com>
2026-04-01 13:30:56 +0000

    feat: new first lvl model with chosen choice variable

--- scripts/spm_first_lvl.sh
+++ scripts/spm_first_lvl.sh
@@ -1,4 +1,4 @@
 module load matlab
 matlab -nodisplay -nosplash -nodesktop -r "\
-run('/home/ubuntu/repos/learning-habits-analysis/matlab/first_lvl/glm2_chosen_all_runs.m'); \
+run('/home/ubuntu/repos/learning-habits-analysis/matlab/first_lvl/glm3_chosen_choice_var.m'); \
 exit;"
